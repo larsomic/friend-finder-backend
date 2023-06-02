194df58250a2159d0a772d695514eddeaf026fc3
larsomic <michael.j.larson@wsu.edu>
2023-06-01 20:19:21 -0700

    Adding initial jwt backend authentication.

---
+++ routes/auth.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('./../models/User');
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      email: req.body.email,
+      password: hashedPassword,
+      name: req.body.name,
+    });
+    const savedUser = await user.save();
+    const token = jwt.sign({ _id: savedUser._id }, 'YOUR_SECRET_KEY');
+    res.cookie('token', token, { httpOnly: true, sameSite: 'None' });
+    res.json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('User not found');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid password');
+
+  const token = jwt.sign({ _id: user._id }, 'YOUR_SECRET_KEY');
+  res.cookie('token', token, { httpOnly: true});
+  res.json({ message: "User logged in successfully" });
+});
+
+router.post('/logout', (req, res) => {
+  res.clearCookie('token');
+  res.send({ status: 'logged out' });
+});
+
+router.get('/status', (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) throw new Error('Unauthenticated');
+    jwt.verify(token, 'YOUR_SECRET_KEY');
+    res.send({ status: 'authenticated' });
+  } catch (err){
+    console.log(err)
+    res.send({ status: 'unauthenticated' });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const app = express()
+app.use(cookieParser());
+
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig));
+
+const port = process.env.SERVER_PORT || 5000;
+app.use(express.json());
+
+// Use the router
+const authRouter = require('./routes/auth');
+app.use('/api/auth', authRouter);
+
+// Start the server
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
