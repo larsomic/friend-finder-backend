fa4c916bc44ca640e0ccad1880f11aa8bff5db0d
larsomic <michael.j.larson@wsu.edu>
2023-06-23 17:17:37 -0700

    Add preferences backend

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const User = require('./../models/User');
+const UserFriendPreferences = require('./../models/UserFriendPreferences');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -46,6 +48,54 @@ router.patch('/', async (req, res) => {
         console.log(err)
         res.send({ status: 'unauthenticated' });
       }
+});
+
+router.get('/userfriendpreferences', async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) throw new Error('unauthenticated');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+    const userIdFromToken = decoded._id;
+    const userFriendPreferences = await UserFriendPreferences.findOne({ user: userIdFromToken });
+    if (!userFriendPreferences) {
+      throw new Error('UserFriendPreferences not found');
+    }
+    res.send(userFriendPreferences);
+  } catch (err) {
+    console.log(err);
+    res.send({ status: 'unauthenticated' });
+  }
+});
+
+router.patch('/userfriendpreferences', async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) throw new Error('unauthenticated');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+    const userIdFromToken = decoded._id;
+    const userFriendPreferences = await UserFriendPreferences.findOne({ user: userIdFromToken });
+    if (!userFriendPreferences) {
+      const newUserFriendPreferences = new UserFriendPreferences({  
+        attractedTo: req.body.attractedTo,
+        religion: req.body.religion,
+        miles: req.body.miles,
+        inPersonPreference: req.body.inPersonPreference,
+        user: userIdFromToken,
+      });
+      await newUserFriendPreferences.save();
+      res.send(newUserFriendPreferences);
+    } else {
+      userFriendPreferences.attractedTo = req.body.attractedTo;
+      userFriendPreferences.religion = req.body.religion;
+      userFriendPreferences.miles = req.body.miles;
+      userFriendPreferences.inPersonPreference = req.body.inPersonPreference;
+      await userFriendPreferences.save();
+      res.send(userFriendPreferences);
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ status: 'unauthenticated' });
+  }
 });
 
 module.exports = router;
